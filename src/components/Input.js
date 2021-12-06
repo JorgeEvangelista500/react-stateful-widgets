375ff1a3bb1a8cd03a408140a46b4b6f4692644a
Jorge Evangelista <jorgeevangelista500@gmail.com>
2021-12-06 18:32:42 -0500

    finished moods js

--- src/components/Input.js
+++ src/components/Input.js
@@ -38,31 +38,31 @@ import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Input() {
   /* STEP 1 */
-const [inputValue, setImputValue] = useState('');
+const [inputValue, setInputValue] = useState('');
 
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
-    const {inputValue} = evt.target;
-    
+    const { value } = evt.target;
+    setInputValue(value)
     /* STEP 4 */
   };
   const reset = () => {
-    setImputValue('')
+    setInputValue('')
     /* STEP 5 */
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: inputValue.length < 10 ? "royalblue" : "crimson"
+    color: inputValue.length < 11 ? "royalblue" : "crimson"
     /* STEP 2 */
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}></div> {inputValue.toUpperCase}
+      <div id='output' style={style}> {inputValue.toUpperCase()} </div> 
       <div>
         <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
